df3e2886ddc0c6cbfef563f90f997c1ccedee624
Sam Schwartz <29schwartz@gmail.com>
2024-08-21 15:18:29 -0500

    New function and rmd for fitting exponential data for ferrozine OD562 assay!

---
+++ Functions/Exponential_Fit.R
@@ -0,0 +1,19 @@
+#Exponential Fit
+
+Exponential <- function(a, r, time){
+  a*exp(r*time)
+}
+
+
+# Exponential Fitting Function ---------------------------------------------
+
+
+Exponential_Fit <- function(.data) {
+  start_values <- c(a = .001, r = 4)
+  nls(.data$OD562 ~ Exponential(a,r,time), data = .data,
+      start = start_values,
+      lower  = c(0,0),
+      control = list(maxiter = 500, minFactor = 1/2000000, warnOnly = TRUE),
+      algorithm = "port",
+      trace = F)
+}
