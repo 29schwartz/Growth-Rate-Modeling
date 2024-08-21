044fd9cca7299982163c71cea9919f99e9532efc
Sam Schwartz <29schwartz@gmail.com>
2024-08-21 16:58:20 -0500

    edited start values

--- Functions/Exponential_Fit.R
+++ Functions/Exponential_Fit.R
@@ -9,11 +9,11 @@ Exponential <- function(a, r, time){
 
 
 Exponential_Fit <- function(.data) {
-  start_values <- c(a = .001, r = 5)
+  start_values <- c(a = .001, r = 4)
   nls(.data$OD562 ~ Exponential(a,r,time), data = .data,
       start = start_values,
       lower  = c(0,0),
-      control = list(maxiter = 500, minFactor = 1/2000000, warnOnly = TRUE),
+      control = list(maxiter = 500, minFactor = 1/20000, warnOnly = TRUE),
       algorithm = "port",
       trace = F)
 }
