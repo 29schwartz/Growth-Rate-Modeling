6f7076ddb2cf5a36532cb2949b14a30d4655169a
Sam Schwartz <29schwartz@gmail.com>
2025-07-17 11:22:24 -0500

    added in a function to select out just the columns we want to fit to improve robustness and minimize confusion. This feeds directly into the gompertz.fit function.

--- Functions/Gompertz_nls.R
+++ Functions/Gompertz_nls.R
@@ -7,17 +7,28 @@ gompertz_model <- function(A, mu, lambda, time){
   A * exp(-exp(-mu * (time - lambda)))
 }
 
-## Gompertz NLS fitting function
 
-gompertz.fit <- function(.data, A_par, mu_par, lambda_par) {
+
+#Select the two columns in the nested tibble you want to use for fitting
+select_nested <- function(df, ...) {
+  cols <- enquos(...)  # capture column names
+  df %>%
+    mutate(nested_growth_data = map(nested_growth_data, ~ select(.x, !!!cols)))
+}
+
+
+
+
+
+gompertz.fit <- function(.data, A_par, mu_par, lambda_par, growth_column) {
   
-  .data %>% dplyr::slice_max(blanked_OD600, n = 5) %>% pull(time) %>% mean() -> time_of_max_val
+  .data %>% dplyr::slice_max(growth_values, n = 5) %>% pull(time) %>% mean() -> time_of_max_val
   
   .data %>% filter(time <= (time_of_max_val + 3)) -> GC
   
   start_values <- c(A = A_par, mu = mu_par, lambda = lambda_par)
   
-  nls(GC$blanked_OD600 ~ gompertz_model(A,mu,lambda,time), data = GC,
+  nls(GC$growth_values ~ gompertz_model(A,mu,lambda,time), data = GC,
       start = start_values,
       lower  = c(0,0,0),
       upper = c(1.8,1.5,22),
