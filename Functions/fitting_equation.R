0fdb658317521733ca4d02bb20b792bac51b7d69
29schwartz <29schwartz@gmail.com>
2026-05-21 22:02:25 -0500

    cleaned

--- Functions/fitting_equation.R
+++ Functions/fitting_equation.R
@@ -9,9 +9,11 @@
 #' @param mu is the growth rate in the exponential phase 
 #' @param .05 is an added coefficient because background absorbance in media only was approximately .05. Improved curve fits
 
-Zwietering_gompertz_model <- function(A, mu, lambda, time){
-  A * exp(-exp((mu/A) * (lambda - time) + 1))
+zwietering_model <- function(A, mu, lambda, time){
+  e_value <- exp(1)
+  return(A * exp( -exp(((mu * e_value) / A) * (lambda - time) + 1)))
 }
+
 
 
 # Zwietering Fitting Function ---------------------------------------------
@@ -23,18 +25,28 @@ Zwietering_gompertz_model <- function(A, mu, lambda, time){
 
 #' @returns a data frame of values less than or equal to the max time which can be fit!
 
-Zwietering.fit <- function(.data, A_par, mu_par, lambda_par) {
+
+#Selects the two columns in the nested tibble you want to use for fitting. You should always have the time column and then the growth_values column!
+select_nested <- function(df, ...) {
+  cols <- enquos(...)  # capture column names
+  df %>%
+    mutate(nested_growth_data = map(nested_growth_data, ~ select(.x, !!!cols)))
+}
+
+#This is the actual fitting function that uses the growth_values and time as well as estimates for the different growth parameters. 
+zwietering.fit <- function(.data, A_par, mu_par, lambda_par, growth_column) {
   
-  .data %>% dplyr::slice_max(OD600, n = 7) %>% pull(time) %>% mean() -> time_of_max_OD
+  .data %>% dplyr::slice_max(growth_values, n = 5) %>% pull(time) %>% mean() -> time_of_max_val
   
-  .data %>% filter(time <= (time_of_max_OD + 1)) -> GC
+  .data %>% filter(time <= (time_of_max_val + 1)) -> GC
   
   start_values <- c(A = A_par, mu = mu_par, lambda = lambda_par)
-  nls(GC$OD600 ~ Zwietering_gompertz_model(A,mu,lambda,time), data = GC,
+  
+  nls(GC$growth_values ~ gompertz_model(A,mu,lambda,time), data = GC,
       start = start_values,
       lower  = c(0,0,0),
-      upper = c(1.7,.35,16),
-      control = list(maxiter = 500, minFactor = 1/2000, warnOnly = TRUE),
+      upper = c(1.8,1.75,22),
+      control = list(maxiter = 500, tol = 1e-10, minFactor = 1e-10, warnOnly = TRUE),
       algorithm = "port",
       trace = F)
 }
